b7c22017c19c7fba6a5c52e3e30cb4d76d2e5477
Gabriel <gabrielbh@uia.no>
2021-02-24 05:54:14 +0100

    Piano

--- Piano1/app/src/main/java/com/example/piano1/FullTonePianoKey.kt
+++ Piano1/app/src/main/java/com/example/piano1/FullTonePianoKey.kt
@@ -3,12 +3,20 @@ package com.example.piano1
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import com.example.piano1.databinding.FragmentFullTonePianoKeyBinding
+import kotlinx.android.synthetic.main.fragment_full_tone_piano_key.view.*
 
 class FullTonePianoKey : Fragment() {
 
     private lateinit var note:String
+    private var _binding:FragmentFullTonePianoKeyBinding? = null
+    private val binding get() = _binding!!
+
+    var onWhiteKeyDown:((note:String) -> Unit)? = null
+    var onWhiteKeyUp:((not:String) -> Unit)? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +26,27 @@ class FullTonePianoKey : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_full_tone_piano_key, container, false)
+
+        _binding = FragmentFullTonePianoKeyBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        view.fullToneKeyBt.setOnTouchListener(object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean{
+                when(event?.action){
+                    MotionEvent.ACTION_DOWN -> this@FullTonePianoKey.onWhiteKeyDown?.invoke(note)
+                    MotionEvent.ACTION_UP -> this@FullTonePianoKey.onWhiteKeyUp?.invoke(note)
+                }
+                return true
+            }
+        })
+        return view
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(note: String) =
             FullTonePianoKey().apply {
                 arguments = Bundle().apply {
                     putString("NOTE", note)
--- Piano1/app/src/main/java/com/example/piano1/PianoLayout.kt
+++ Piano1/app/src/main/java/com/example/piano1/PianoLayout.kt
@@ -5,30 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.piano1.databinding.FragmentPianoBinding
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_piano.view.*
 
 
 class PianoLayout : Fragment() {
 
-    private val fullTones = listOf("C","D","E","F","G","A","B","C","D","E","F","G")
+    private var _binding: FragmentPianoBinding? = null
+    private val binding get() = _binding!!
+    private val fullTones = listOf("C","D","E","F","G","A","B","C2","D2","E2","F2","G2")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
+
+        _binding = FragmentPianoBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        val fm = childFragmentManager
+        val ft = fm.beginTransaction()
+
+        fullTones.forEach {
+            val fullTonePianoKey = FullTonePianoKey.newInstance(it)
+            fullTonePianoKey.onWhiteKeyDown = { println("Piano key down, note $it") }
+            fullTonePianoKey.onWhiteKeyUp = { println("Piano key up, note $it") }
+            ft.add(view.FullToneKeyLinearLayout.id, fullTonePianoKey, "note_$it")
+
+        }
+
+        ft.commit()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_piano, container, false)
+        //return inflater.inflate(R.layout.fragment_piano, container, false)
+        return view
     }
 
-    companion object {
+    /*companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             PianoLayout().apply {
-                
+
             }
-    }
+    }*/
 }
